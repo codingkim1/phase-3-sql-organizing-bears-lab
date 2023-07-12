1c037501d9ed8ae6ecd328cca10dd36f48b67f9d
Alex <alexkimanthi92@gmail.com>
2023-07-12 20:51:00 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+
+-- Inserting 8 bears
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES
+    (1,'Bear 1', 5, 'Male', 'Brown', 'Calm', true),
+    (2,'Bear 2', 3, 'Female', 'Black', 'Aggressive', true),
+    (3,'Bear 3', 8, 'Male', 'Blonde', 'Playful', true),
+    (4,'Bear 4', 4, 'Female', 'Brown', 'Timid', true),
+    (5,'Bear 5', 6, 'Male', 'White', 'Curious', true),
+    (6,'Bear 6', 2, 'Female', 'Brown', 'Energetic', true),
+    (7,'Bear 7', 7, 'Male', 'Black', 'Relaxed', true),
+    (8,NULL, 10, 'Male', 'Brown', 'Mysterious', true);
+
+ 
